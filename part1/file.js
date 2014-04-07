4d13a2881bce4eea74affb4195bffa0d6132ad08
Dan Perata <djperata@aol.com>
2014-04-07 11:28:04 -0600

    created new directory and transferred files into it.

---
+++ part1/file.js
@@ -0,0 +1,15 @@
+alert('Hello Boulder!');
+
+var sunny = true;
+var carAvail = false;
+
+if(sunny){
+	console.log("I'm going to ride my bike to work");
+}
+
+else if(carAvail){
+		console.log("Drive to work");
+	}
+else{
+		console.log("Take the bus");
+	}
